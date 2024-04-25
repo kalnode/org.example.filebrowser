95b4d903b94d9f5756789210289e19ba5f1a2792
Kal <kounapuu@gmail.com>
2024-04-25 12:38:19 -0400

    Actions: global event trigger

--- src/Window.js
+++ src/Window.js
@@ -1,3 +1,5 @@
+import Gio from 'gi://Gio';
+import GLib from 'gi://GLib';
 import GObject from 'gi://GObject';
 import Gtk from 'gi://Gtk';
 
@@ -6,12 +8,29 @@ export const Window = GObject.registerClass({
     Template: 'resource:///org/example/filebrowser/ui/Window.ui',
     InternalChildren: ['viewStack'],
 }, class extends Gtk.ApplicationWindow {
+    constructor(params={}) {
+        super(params);
+        this.#setupActions();
+    }
+
     vfunc_close_request() {
         super.vfunc_close_request();
         this.run_dispose();
     }
 
-    onWelcomeButtonClicked(_widget) {
-        this._viewStack.visibleChildName = 'files';
+    #setupActions() {
+        // Create the action
+        const changeViewAction = new Gio.SimpleAction({
+            name: 'change-view',
+            parameterType: GLib.VariantType.new('s'),
+        });
+
+        // Connect to the activate signal to run the callback
+        changeViewAction.connect('activate', (_action, params) => {
+            this._viewStack.visibleChildName = params.unpack();
+        });
+
+        // Add the action to the window
+        this.add_action(changeViewAction);
     }
 });
